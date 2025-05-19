9bf2efe516043fde387f22c455ef2b96135ac226
gilang-as <gilangas@outlook.com>
2025-05-19 20:57:20 +0700

    Upgrade Ant Design & Next JS

--- src/i18n.ts
+++
@@ -1,5 +0,0 @@
-import { getRequestConfig } from 'next-intl/server';
-
-export default getRequestConfig(async ({ locale }) => ({
-  messages: (await import(`./data/languages/${locale}.json`)).default,
-}));

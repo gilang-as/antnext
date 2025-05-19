1698441e2ddfd5982de08e1f1d519f1a28257162
gilang-as <gilangas@outlook.com>
2025-05-19 20:44:56 +0700

    Upgrade Ant Design & Next JS

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -4,7 +4,6 @@ import { routing } from "./routing";
 export default getRequestConfig(async ({ requestLocale }) => {
     let locale = await requestLocale;
 
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     if (!locale || !routing.locales.includes(locale as any)) {
         locale = routing.defaultLocale;
     }
